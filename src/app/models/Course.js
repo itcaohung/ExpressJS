9d7bd9b0f2508516f813df86536756eb77fdbe43
Jay Cao <itcaohung@gmail.com>
2021-06-07 22:47:44 +0700

    install mongoose-slug-generator

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
@@ -6,6 +10,7 @@ const Course = new Schema({
     description: {type: String},
     image: {type: String},
     videoId: {type: String},
+    slug: {type: String, slug: 'name', unique: true},
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now}
   });
