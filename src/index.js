4a855943bf326b7be68f63ed8bea40c849aa0b01
Jay Cao <itcaohung@gmail.com>
2021-06-07 08:45:27 +0700

    MVC, controller

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const path = require('path');
 const handlebars = require("express-handlebars");
 
+const route = require('./routes');
+
 const app = express()
 const port = 3000
 
@@ -18,13 +20,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const newRouter = require('./news');
+const siteRouter = require('./site');
+
+function route(app) {
+
+    app.get('/news', newRouter);
+
+    app.get('/', siteRouter);
+}
+
+module.exports = route;
